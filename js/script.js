2af3e0ccfd224ec60f0bd170264a0a441e1aa922
Asilbek <asilbekkaromatov2@gmail.com>
2026-05-25 14:57:38 +0500

    lesson started

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// console.log('Dars boshlandi')
+
+/*console.log(3.14568 * 5 * 3)
+console.log(7 * 3.14568 * 2)
+
+const pi = 3.14568
+console.log(pi * 5 * 3)
+console.log(7 * pi * 2)*/
+
+/*
+let oylikMaosh = 45000
+let daromadSoligi = 0.12 // 12%
+let soliqMiqdori = oylikMaosh * daromadSoligi
+
+console.log(soliqMiqdori)*/
+
