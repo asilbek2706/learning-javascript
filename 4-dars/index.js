412c535dfc5c93c8771f07155ccf56da1999c93c
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-02 14:27:23 +0500

    if...else if...else

---
+++ 4-dars/index.js
@@ -0,0 +1,89 @@
+// const balandlik = 150;
+
+// if(balandlik >= 160){
+//     console.log("Siz basketbol o'ynashga mos kelasiz");
+// }
+
+// let svetafor = "qizil";
+
+// if (svetafor === "qizil") {
+//   console.log("Harakatlanish mumkin emas!");
+// }
+
+
+// const yosh = 16;
+
+// if (yosh >= 18) {
+//     console.log("Siz haydovchilik guvohnomasi olishingiz mumkin.");
+// } else {
+//     console.log("Siz hali voyaga yetmagansiz.");
+// }
+
+// const ball = 95;
+
+// if (ball >= 90) {
+//     console.log("Sizning bahoingiz: A");
+// } else if (ball >= 80) {
+//     console.log("Sizning bahoingiz: B");
+// } else if (ball >= 70) {
+//     console.log("Sizning bahoingiz: C");
+// } else {
+//     console.log("Siz imtihondan o'ta olmadingiz.");
+// }
+
+// const parolKiritildi = true;
+// const parolTogri = false;
+
+// if (parolKiritildi) {
+//     if (parolTogri) {
+//         console.log("Tizimga xush kelibsiz!");
+//     } else {
+//         console.log("Xato parol kiritildi.");
+//     }
+// } else {
+//     console.log("Iltimos, parolni kiriting.");
+// }
+
+// const yosh = 22;
+// const IDKartaBor = true;
+
+// // AND operatoriga misol (Ikkala shart ham bajarilishi kerak)
+// if (yosh >= 18 && IDKartaBor) {
+//     console.log("Klubga kirishga ruxsat.");
+// } else {
+//     console.log("Guvohnoma yo'q yoki yosh yetarli emas.");
+// }
+
+// const damOlishKuni = true;
+// const obHavoYaxshi = false;
+
+// // OR operatoriga misol (Kamida bittasi bajarilsa yetarli)
+// if (damOlishKuni || obHavoYaxshi) {
+//     console.log("Aylanib kelamiz.");
+// } else {
+//     console.log("Uyda qolib kod yozamiz.");
+// }
+
+// (10-20) , (20-30), (30-40)
+
+// let son1 = 20;
+// let son2 = 10;
+// let natija = son1 + son2;
+
+// if(natija > 10 && natija <= 20){
+//     console.log("Natija 10 dan katta va 20 ga teng yoki kichik");
+// } else if(natija > 20 && natija <= 30){
+//     console.log("Natija 20 dan katta va 30 ga teng yoki kichik");
+// } else if(natija > 30 && natija <= 40){
+//     console.log("Natija 30 dan katta va 40 ga teng yoki kichik");
+// } else {
+//     console.log("Natija 10 dan kichik yoki 40 dan katta");
+// }
+
+const yosh = 18
+
+if(yosh >= 18){
+    console.log("Siz haydovchilik guvohnomasi olishingiz mumkin.");
+} else {
+    console.log("Siz hali voyaga yetmagansiz.");
+}
