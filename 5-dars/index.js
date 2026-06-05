086599c2f86042eb8d19fd4343d5f8418eb6975e
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-05 17:21:54 +0500

    while and for loop

--- 5-dars/index.js
+++ 5-dars/index.js
@@ -1,17 +1,18 @@
-// const tugma = 4
+// const tugma = '2';
 
-// switch(tugma){
+// switch (tugma) {
 //     case 1:
-//         console.log("Tugma 1 bosildi");
+//         console.log("Suv quyilmoqda...");
 //         break;
-//     case 2:
-//         console.log("Tugma 2 bosildi");
+//     case '2':
+//         console.log("Kofe quyilmoqda..."); // Dastur shu yerdan kiradi
+//         break
+//     case 3:
+//         console.log("Choy quyilmoqda...");  // break yo'qligi uchun buni ham bajaradi!
 //         break;
 //     case 4:
-//         console.log("Tugma 4 bosildi");
+//         console.log("Sharbat quyilmoqda...");// buni ham bajaradi!
 //         break;
-//     default:
-//         console.log("Bunday tugma mavjud emas");
 // }
 
 // const rol = "mehmon"; // Tizimda admin, moderator va foydalanuvchi degan rollar bor
@@ -26,46 +27,40 @@
 //     case "foydalanuvchi":
 //         console.log("Siz faqat postlarni o'qiy olasiz.");
 //         break;
-//     case "mehmon":
-//         console.log("Siz tizimga kirish uchun ro'yxatdan o'tishingiz kerak.");
-//         break;
+   
 //     default:
-//         console.log("Tizimga bunday roldagi odam kira olmaydi!");
-// }
-// const tugma = 'ikkinchi'
-
-// switch(tugma){
-//     case 'birinchi':
-//         console.log("Tugma 1 bosildi");
-//         break;
-//     case 'ikkinchi':
-//         console.log("Tugma 2 bosildi");
-//         break;
-//     case 'uchinchi':
-//         console.log("Tugma 3 bosildi");
-//         break;
-//     default:
-//         console.log("Bunday tugma mavjud emas");
+//         console.log("Tizimga bunday roldagi odam kira olmaydi!"); // Aynan shu kod ishlaydi
 // }
 
-const oy = "Dekabr";
+// const oy = "Yanvar";
 
-switch (oy) {
-    case "Dekabr":
-    case "Yanvar":
-    case "Fevral":
-        console.log("Qish fasli");
-        break;
-    case "Mart":
-    case "Aprel":
-    case "May":
-        console.log("Bahor fasli");
-        break;
-    case "Iyun":
-    case "Iyul":
-    case "Avgust":
-        console.log("Yoz fasli"); // Iyul kelganda break bo'lguncha tushadi va shu yerda to'xtaydi
-        break;
-    default:
-        console.log("Noto'g'ri oy kiritildi");
-}
+// switch (oy) {
+//     case "Dekabr":
+//     case "Yanvar":
+//     case "Fevral":
+//         console.log("Qish fasli");
+//         break;
+//     case "Mart":
+//     case "Aprel":
+//     case "May":
+//         console.log("Bahor fasli");
+//         break;
+//     case "Iyun":
+//     case "Iyul":
+//     case "Avgust":
+//         console.log("Yoz fasli"); // Iyul kelganda break bo'lguncha tushadi va shu yerda to'xtaydi
+//         break;
+//     default:
+//         console.log("Noto'g'ri oy kiritildi");
+// }
+
+
+// let baho = 5; // Number (Son) bo'lib turibdi
+
+// switch (baho) {
+//     case 5: // Number (Son)
+//         console.log("Alo");
+//         break;
+//     default:
+//         console.log("Bahoni aniqlab bo'lmadi"); // Aynan shu ishlaydi! faqat ===
+// }
---
+++ 6-dars/index.js
@@ -0,0 +1,34 @@
+// let a = 1;
+
+// for (a; a <= 10; a += 2) {
+//   console.log(`${a}. Salom`);
+// }
+
+// let i = 1
+
+// for (i; i <= 10; i++) {
+//     if(i === 6){
+//         console.log('6 ga yetdik');
+//         break;
+//     }
+//     console.log(`${i}. Salom`);
+// }
+
+// let i = 1;
+
+// for (i; i <= 10; i++) {
+//     if(i % 2 !== 0){
+//         console.log('juft son tashlab ketdik');
+//         continue;
+//     }
+//     console.log(`${i}  son`);
+// }
+
+// let sirliSoz = "kod";
+// let foydalanuvchiKiritmasi = "";
+
+// while (foydalanuvchiKiritmasi !== sirliSoz) {
+//   foydalanuvchiKiritmasi = console.log("Sirli so'zini toping:");
+// }
+
+// alert("Tabriklaymiz! To'g'ri topdingiz.");
