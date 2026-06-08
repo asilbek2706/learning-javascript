b4eeb5a661335c90565ae9a14cfc768b3daa33b3
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-08 14:51:21 +0500

    Functions explanations

---
+++ 7-dars/index.js
@@ -0,0 +1,56 @@
+// Functions
+// console.log(calcSquare(5)); // 25
+
+/**Clean code
+ *
+ * 1. calc, ckeck, get, set, create, make, build, generate
+ * 2. camelCase
+ * 3. Function should do one thing
+ * 4. Function should be small
+ * 5. Function should return something
+ */
+
+// Function declaration
+// function calcSum(a, b){
+//     console.log(`The sum is ${a} + ${b} = ${a + b}`);
+//     return a + b;
+//     console.log('This will not be executed');
+// }
+
+// let result = calcSum(Number(5), Number(10));
+// console.log(result);
+
+// Function expression
+// let user = false;
+
+// const checkUser = function () {
+//   if (user) console.log("User bor");
+//   else console.log("User yoq");
+// };
+
+// checkUser();
+
+// const congratulations = function (name) {
+//   console.log(
+//     `Tabriklaymiz ${name}! Siz bu darsni muvaffaqiyatli yakunladingiz!`,
+//   );
+// };
+
+// // Arrow function - Zamonaviy va qisqaroq funksiya yozish usuli
+// const congratulationsArrow = (name) => console.log(`Tabriklaymiz ${name}! Siz bu darsni muvaffaqiyatli yakunladingiz!`,);
+
+// congratulationsArrow("Asilbek");
+
+// const calcArea = (radius) =>  Math.PI * radius ** 2
+
+// const area = calcArea(5);
+// console.log(`Aylananing yuzi: ${area}`);
+
+
+// function calcSquare(num){
+//     return num ** 2;
+// }
+
+// const square = calcSquare(+prompt('Son kiriting:'));
+// console.log(`Kiritilgan sonning kvadrati: ${square}`);
+
